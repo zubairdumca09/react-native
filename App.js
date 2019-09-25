51c21603809cfa43dc017d08bf542d542945afae
saurabh aggarwal <saurabh@saurabhs-MacBook-Pro.local>
2019-09-25 23:02:22 +0530

    changes

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [  text, setText ] = useState('Open up App.js to start working on your app!');
+  const [  text, setText ] = useState('');
+  const [ listItem, setListItem ] = useState([]);
+  const handleInput = (text) => {
+    setText(text);
+  }
+  const addList = () => {
+    const list = [...listItem, text];
+    console.log(list);
+    setListItem(list);
+  }
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <Button title="Change Text" onPress={()=>{setText("Text Changed")}}/>
+    <View style={styles.screen}>
+      <TextInput
+        placeholder="text"
+        onChangeText={handleInput}
+        value={text}
+        style={{borderBottomWidth: 1, width: '100%', borderBottomColor: 'black', marginBottom: 50}}
+      />
+      <Button title="Change Text" onPress={addList}/>
+      <View>
+        {
+          listItem.map((item)=><View  key={item} style={styles.listItem}><Text>{item}</Text></View>)
+        }
+      </View>  
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  listItem: {
+    padding: 10,
+    marginVertical: 5,
+    borderColor: 'black',
+    backgroundColor: '#d6d6d6',
+
+  }
 });
